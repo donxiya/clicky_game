a53060eea82140cf9f09344f0cc772f1648f8b55
donxiya <31514669+donxiya@users.noreply.github.com>
2019-08-25 18:48:37 -0500

    finished set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Box from "./components/Box";
+import Wrapper from "./components/Wrapper";
+import Header from "./components/Header";
+import cards from "./boxList.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    boxlist,
+    score: 0,
+    message: ""
+  };
+
+  gameEnd = () => {
+    this.setState({ message: "Game Over" });
+    this.setState({ score: 0 });
+    return true;
+  }
+
+  clickCount = id => {
+    this.state.boxlist.find((any, i) => {
+      if (any.id === id) {
+        if (boxlist[i].select === false) {
+          boxlist[i].select = true;
+          this.setState({ score: this.state.score + 1 }, function () {
+            console.log(this.state.score);
+          });
+          this.state.cards.sort(() => Math.random() - 0.5)
+          return true;
+        } else {
+          this.gameOver();
+        }
+      }
+    });
+  }
+  render() {
+    return (
+      <Wrapper>
+        <Header score={this.state.score}
+          highscore={this.state.message}>
+          Bird Watch</Header>
+        {this.state.cards.map(card => (
+          <Box
+            clickCount={this.clickCount}
+            id={box.id}
+            key={box.id}
+            image={box.image}
+          />
+        ))}
+      </Wrapper>
+      <div>Source: <a href>https://www.audubon.org</a></div>
+    );
+  }
 }
 
 export default App;
