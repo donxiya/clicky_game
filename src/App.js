e2927122ccc7754124462b61ae9c6821d1d62aac
donxiya <31514669+donxiya@users.noreply.github.com>
2019-08-25 20:09:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
             clickCount={this.clickCount}
             id={box.id}
             key={box.id}
-            image={require(box.image)}
+            image={box.image}
           />
         ))}
         <div>Source: <a href>https://www.audubon.org</a></div>
