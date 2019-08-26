4fd5cb61e31166d292b95aec6d2bd0c8e7380e5d
donxiya <31514669+donxiya@users.noreply.github.com>
2019-08-25 19:53:38 -0500

    Update Box.js

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -2,7 +2,7 @@
 import React from "react";
 import "./Box.css";
 
-const Card = props => (
+const Box = props => (
     <div className="box" onClick={() => props.clickCount(props.id)}>
       <div className="img-box">
         <img alt={props.name} src={props.image} />
