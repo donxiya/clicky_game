4edb1dd5d91a4b5b3d550fceac48721c9ad2b2c5
donxiya <31514669+donxiya@users.noreply.github.com>
2019-08-25 20:48:04 -0500

    img path

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -5,7 +5,7 @@ import "./Box.css";
 const Box = props => (
     <div className="box" onClick={() => props.clickCount(props.id)}>
       <div className="img-box">
-        <img alt={props.name} src={require(`../assets/images/1.jpg`)} />
+        <img alt={props.name} src={props.image} />
       </div>
     </div>
   );
